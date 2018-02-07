9c8f5792d02dffa79e43876eb0c56eae0c3bf7b8
ben-foster <benfoster1093@gmail.com>
2018-02-06 22:58:46 -0500

    Add profile.js, which gets user profile data from Treehouse API. Add empty template files.

---
+++ profile.js
@@ -0,0 +1,63 @@
+const EventEmitter = require("events").EventEmitter;
+const https = require("https");
+const http = require("http");
+const util = require("util");
+
+function Profile(username){
+	EventEmitter.call(this);
+
+	//stored in a variable to eliminate potential namespace conflicts with "this" keyword in the following code
+	const self = this;
+
+	try{
+		const request = https.get(`https://teamtreehouse.com/${username}.json`, (response) =>{
+
+			const statusCode = response.statusCode;
+
+			console.log(statusCode);
+			console.log(http.STATUS_CODES[statusCode]);
+			
+			let responseBody = "";
+
+			if(statusCode !== 200){
+				request.abort();
+
+				self.emit("error", new Error(`There was an error getting profile for ${username} [${statusCode}]`));
+			}
+
+			response.on("data", chunk => {
+				responseBody += chunk.toString();
+				self.emit("data", chunk);
+			});
+
+			response.on("end", () => {
+				try{
+					const profile = JSON.parse(responseBody);
+					self.emit("end", profile);
+				} catch (error){
+					self.emit("error", error);
+				}
+			});
+
+			response.on("error", printError);
+
+		});
+
+		request.on("error", printError);
+	} catch (error){
+		printError(error);
+	}
+}
+
+const printMessage = (username, badgeCount, points) => {
+	const message = `${username} has ${badgeCount} total badges and ${points} points in JavaScript`;
+	console.log(message);
+}
+
+const printError = (error) =>{
+	console.error(error.message);
+}
+
+util.inherits(Profile, EventEmitter);
+
+module.exports = Profile;
--- router.js
+++ router.js
@@ -1,3 +1,4 @@
+const Profile = require("./profile");
 
 const home = (request, response) => {
 	if(request.url === "/"){
@@ -5,7 +6,7 @@ const home = (request, response) => {
 		response.write("Header\n");
 		response.write("Search\n");
 		response.end("Footer\n");
-	} //else if( request.url === '/')
+	}
 }
 
 const user = (request, response) => {
@@ -14,9 +15,32 @@ const user = (request, response) => {
 	if(username.length > 0){
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write("Header\n");
-		response.write(username + "\n");
-		response.end("Footer\n");
-	} 
+	
+		const userProfile = new Profile(username);
+
+		userProfile.on("end", (profileJSON) => {
+			
+			//store the values we need
+			const values = {
+				avatarUrl: profileJSON.gravatar_url,
+				username: profileJSON.profile_name,
+				badges: profileJSON.badges.length,
+				javascriptPoints: profileJSON.points.JavaScript
+			}
+
+			response.write(`${values.username} has ${values.badges} badges and ${values.javascriptPoints} JavaScript points.\n`);
+			console.dir(profileJSON);
+			response.end("Footer\n");
+
+		});
+
+		userProfile.on("error", (error) => {
+			console.error(error.message);
+			response.end("Footer\n");
+		});
+	
+
+	}
 }
 
 module.exports.home = home;
